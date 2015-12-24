69477eca55aff0cf178ceced521608e832e17e18
Eugene Zaretskiy <ujinius@gmail.com>
2015-12-24 20:36:39 +0200

    remove zip code unique validation

--- app/models/zip_code_range.rb
+++ app/models/zip_code_range.rb
@@ -1,7 +1,7 @@
 class ZipCodeRange < ActiveRecord::Base
   self.table_name = 'zip_code_ranges'
-  validates :zip_from, presence: true, numericality: true, uniqueness: true
-  validates :zip_to, presence: true, numericality: true, uniqueness: true
+  validates :zip_from, presence: true, numericality: true #, uniqueness: true
+  validates :zip_to, presence: true, numericality: true #, uniqueness: true
 
   validates_length_of :zip_from, :minimum => 4, :maximum => 5, :allow_blank => false
   validates_length_of :zip_to, :minimum => 4, :maximum => 5, :allow_blank => false
